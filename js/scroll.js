cb05c2ce3be3bd6d5e2e97b84a9646119de1a9e2
kyphong2k <80156896+kyphong2k@users.noreply.github.com>
2022-04-03 16:54:11 +0700

    innitial commit

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".navbar").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in css)
+       $(".navbar").removeClass("active");
+    }
+});
